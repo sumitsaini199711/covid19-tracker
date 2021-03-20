055d963cfd1e9f446f6388d0d65a287aef245c29
Sumit Saini <sumitsaini199711@gmail.com>
2021-03-21 00:43:51 +0530

    Added Country dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import './App.css';
+import "./App.css";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
 
 function App() {
   return (
     <div className="app">
-      Covid-19 Tracker
+      <div className="app__header">
+        <h1>COVID-19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
